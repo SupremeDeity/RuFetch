d612fc50afca31fc01ff262bf997bddf035f5005
SupremeDeity <sheikhmohsin706@gmail.com>
2021-02-14 19:46:03 +0500

    Added Color support + Fixed Memory Ouput

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ansi_term::Color::*;
 use std::process::Command;
 use std::str;
 use sysinfo::{System, SystemExt};
@@ -15,6 +16,7 @@ use sysinfo::{System, SystemExt};
 
 fn main() {
     let sys = System::new_all();
+    //let enabled = ansi_term::enable_ansi_support();
 
     let mut user_out = if cfg!(target_os = "windows") || cfg!(target_os = "linux") {
         // linux, windows
@@ -32,37 +34,51 @@ fn main() {
     };
 
     let os = sys.get_name();
+    let host_name = sys.get_host_name();
     //let uptime_sec = sys.get_uptime();
     let uptime_min: f64 = sys.get_uptime() as f64 / 60 as f64;
     //let uptime_hour: f64 = sys.get_uptime() as f64 / 3600 as f64;
-    let host_name = sys.get_host_name();
     let kernel_ver = sys.get_kernel_version();
-    let total_mem = sys.get_total_memory();
-    let total_swap = sys.get_total_swap();
-    let used_mem = sys.get_used_memory();
-    let used_swap = sys.get_used_swap();
+    let total_mem = sys.get_total_memory() as f64 / 1e+6;
+    let total_swap = sys.get_total_swap() as f64 / 1e+6;
+    let used_mem = sys.get_used_memory() as f64 / 1e+6;
+    let used_swap = sys.get_used_swap() as f64 / 1e+6;
 
     if let Some(host_name) = &host_name {
-        println!("{}@{}", user, host_name);
+        println!(
+            "{}@{}",
+            Blue.bold().paint(user),
+            Blue.bold().paint(host_name)
+        );
     }
     println!("{:-<1$}", "", 30);
 
     if let Some(os) = &os {
-        println!("OS: {}", os);
+        println!("{} {}", Blue.bold().paint("OS:"), os);
+    }
+
+    if let Some(host_name) = &host_name {
+        println!("{} {}", Blue.bold().paint("Host:"), *host_name);
     }
 
     //println!("Uptime: {} sec(s)", uptime_sec);
-    println!("Uptime: {:.2} min(s)", uptime_min);
+    println!("{} {:.2} min(s)", Blue.bold().paint("Uptime:"), uptime_min);
     //println!("Uptime: {:.2} hour(s)", uptime_hour);
 
-    if let Some(host_name) = &host_name {
-        println!("Host Name: {}", *host_name);
-    }
-
     if let Some(kernel_ver) = &kernel_ver {
-        println!("Kernel Version: {}", *kernel_ver);
+        println!("{} {}", Blue.bold().paint("Kernel Version:"), *kernel_ver);
     }
 
-    println!("Memory: {} / {}", used_mem, total_mem);
-    println!("Swap: {} / {}", used_swap, total_swap);
+    println!(
+        "{} {:.2} GB / {:.2} GB",
+        Blue.bold().paint("Memory:"),
+        used_mem,
+        total_mem
+    );
+    println!(
+        "{} {:.2} GB / {:.2} GB",
+        Blue.bold().paint("Swap:"),
+        used_swap,
+        total_swap
+    );
 }
